ff3aad8c629168fcfb35ced1af0e9f4e64b8a6e4
SantinoG19 <gallis@udesa.edu.ar>
2023-09-04 17:05:55 -0300

    carpetas

---
+++ proyecto/src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home () {
+    return (
+        <React.Fragment>
+            <h1>Home</h1>
+            <p>Descripcion</p>
+            <h2> Mas populares</h2>
+        </React.Fragment>
+    )
+}
+export default Home;
