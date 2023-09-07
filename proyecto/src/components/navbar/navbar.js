b3c78f184b2a189bd88223afe45105a1a24cb044
SantinoG19 <gallis@udesa.edu.ar>
2023-09-07 14:24:18 -0300

    css

--- proyecto/src/components/navbar/navbar.js
+++ proyecto/src/components/navbar/navbar.js
@@ -3,7 +3,25 @@ import React from "react";
 function Navbar() {
     return(
         <React.Fragment>
-            <p> Pagina de Ver Detalle pelicula</p>
+            <header>
+        <div className="contenedor">
+            
+            <a href="./index.html"  className="logo">FLIXER </a>
+            <div id='search-box'>
+                <form action='./search-result.html' id='search-form' method='get' target='_top'>
+                <input id='search-text' name='search' placeholder='Busca en Flixer' type='text'/>
+                <button id='search-button' type='submit'> Busca</button>
+                
+                </form>
+                </div>
+              
+            <nav>
+                <a  href="./index.html">Inicio</a>
+                <a href="./favoritos.html">Favoritos</a>
+                <a href="./generos.html">Generos</a>
+            </nav>
+        </div>
+    </header>
         </React.Fragment>
     );   
 }
--- proyecto/src/screens/Home/Home.js
+++ proyecto/src/screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import navbar from "../../components/Navbar/navbar"
 
 function Home () {
     return (
@@ -7,6 +7,7 @@ function Home () {
             <h1>Home</h1>
             <p>Descripcion</p>
             <h2> Mas populares</h2>
+            <h2>Tendencias</h2>
         </React.Fragment>
     )
 }
