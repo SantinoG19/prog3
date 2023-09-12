037fb5dc6727311772392070757209d3005693b2
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-09-12 20:02:12 -0300

    detalle pelis

--- proyecto/src/screens/DetallePeli/DetallePeli.js
+++ proyecto/src/screens/DetallePeli/DetallePeli.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-function DetallePeli() {
-    return(
-        <React.Fragment>
-            <p> Pagina de Ver Detalle pelicula</p>
-        </React.Fragment>
-    );   
+
+class DetallePeli extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detalle: {},
+    };
+  }
+    componentDidMount() {
+        let idPeli= this.props.match.params
+
+        fetch(`https://api.themoviedb.org/3/movie/${idPeli}?api_key=925f4b20191d3e6290b49bd816600eda&language=en-US`)
+        .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          detalle: data,
+        })
+      )
+      .catch((error) => console.log(error));
+
+    }
+  render() {
+    let {detalle}= this.state;
+    console.log(detalle);
+    return (
+      <React.Fragment>
+        
+        <div>
+          <h1> Detalle de peliculas</h1>
+          <img className="" src={`https://image.tmdb.org/t/p/w500${detalle.poster_path}`} alt={detalle.original_title} />
+        </div>
+      
+      </React.Fragment>
+    );
+  }
+  
 }
-
 
 
 
