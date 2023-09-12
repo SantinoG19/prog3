a30382e5d10e518f204cd5453cf6856c83bfa56f
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-09-12 17:52:48 -0300

    pagina error

--- proyecto/src/screens/NoEncontrada/NoEncontrada.js
+++ proyecto/src/screens/NoEncontrada/NoEncontrada.js
@@ -3,13 +3,20 @@ import React from "react";
 function NoEncontrada() {
     return(
         <React.Fragment>
-            <p> Pagina de NoEncontrada</p>
+           <div>
+
+                <h1>
+                    Error 404 Not Found
+                </h1>
+
+
+                <a class="" href="/">Haz click aquí para volver a home</a>
+
+                </div>
+
         </React.Fragment>
     );   
 }
-
-
-
 
 
 export default NoEncontrada
