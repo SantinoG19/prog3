7efdb95b59ebd99c3794be72b496b5eb7589931f
SantinoG19 <gallis@udesa.edu.ar>
2023-09-12 21:20:40 -0300

    fav

--- proyecto/src/components/Card/CardMovie.js
+++ proyecto/src/components/Card/CardMovie.js
@@ -6,8 +6,39 @@ let img = 'https://image.tmdb.org/t/p/w500'
 class Card extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
-    };  render(){
+        this.state = {
+            favTexto:" Agregar a favoritos",
+        }
+    }; 
+
+    componentDidMount(){
+        let confir1= localStorage.getItem('favoritos')
+        if (confir1 =! null){
+            let confir= JSON.parse(confir1)
+        
+        
+            if(confir.includes(this.props.contentPeli.id)){
+                this.setState({
+                    favTexto: 'Quitar de favoritos'
+
+                })
+            }}
+    }
+    
+    modificarFav(id){
+
+        let favoritos = []
+        favoritos.push(id)
+        let favString = JSON.stringify(favoritos)
+        localStorage.setItem('favoritos', favString)
+
+        this.setState({
+            favTexto: 'quitar de favoritos'
+        })
+    }
+
+
+    render(){
       
         return (
             
@@ -18,7 +49,7 @@ class Card extends Component {
                 <img className='a ' src= {img + this.props.contentPeli.poster_path}  alt={this.props.contentPeli.title}/>
                 <h3 className='margen'>{this.props.contentPeli.title}  </h3>
                 
-                <button type='button' className='boton'>AGREGAR A FAVORITOS</button>
+                <button onClick={()=> this.modificarFav(this.props.contentPeli.id)} type='button'>{this.state.favTexto}</button>
                 
 
                 
