5f75e62075479a2d3047b986cda746ffdeb6250e
SantinoG19 <gallis@udesa.edu.ar>
2023-09-14 15:15:54 -0300

    cambios

--- proyecto/src/screens/Favoritos/Favoritos.js
+++ proyecto/src/screens/Favoritos/Favoritos.js
@@ -23,8 +23,8 @@ let favoritos = [];
 if (guardados !== null) {
    favoritos = JSON.parse(guardados);
 }
-
-fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e&language=en-US&page=1`)
+let urlM = `https://api.themoviedb.org/3/movie/top_rated?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e&language=en-US&page=1`
+fetch(urlM)
 .then((response) => response.json())
 .then((data) => {
    let pelis = data.results.filter((contenido) => {
@@ -45,7 +45,8 @@ let serie=[]
   if (traer !== null){
     favoritosSerie=JSON.parse(traer)
   }
-fetch('https://api.themoviedb.org/3/tv/popular?api_key=a3c55e0abc72e6abaa573f83ee40635f&language=en-US&page=1')
+let url = 'https://api.themoviedb.org/3/tv/popular?api_key=a3c55e0abc72e6abaa573f83ee40635f&language=en-US&page=1'
+fetch(url)
 .then((response)=> response.json())
 .then((data)=>{
   let series =data.results.filter((contenido)=>{
@@ -71,26 +72,25 @@ fetch('https://api.themoviedb.org/3/tv/popular?api_key=a3c55e0abc72e6abaa573f83e
     console.log(this.state.pelis);
     console.log(this.state.series)
     return (
-      <React.Fragment>
-        
-        <main>
-    
-          
-          <h1 class="maintitulos"> Peliculas Favoritas </h1> 
-          <CardsContainer infoPeli={this.state.pelis} />    
-          
-        <h1 class="maintitulos">Series Favoritas</h1>
-        <CardsContainer informacionSerie={this.state.series}/>
-        
-    
-        </main>
-      
-      </React.Fragment>
-    );
-  }
-  
-}
 
+      <section className='item-row'>
+      {this.state.pelis.length <= 0 ? (
+        <p className="fabi">no tiene favs</p>
+      ) : (
+        <React.Fragment>
+          <main>
+            <h1 className="maintitulos">Peliculas Favoritas</h1>
+            <CardsContainer infoPeli={this.state.pelis} />
+            
+            <h1 className="maintitulos">Series Favoritas</h1>
+            <CardsContainer informacionSerie={this.state.series} />
+          </main>
+        </React.Fragment>
+      )}
+    </section>
+  );
+}
+}
 
 
 
